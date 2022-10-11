297c4b839b94442e2eefd89373af80a7fcd101aa
dnlstronge <114242578+dnlstronge@users.noreply.github.com>
2022-10-11 14:15:58 +0100

    Create basebuild.js
    Created basic prompt code which asks for case issue and returns generic response

---
+++ basebuild.js
@@ -0,0 +1,5 @@
+let topic = prompt("Case issue?")
+      if (topic != null) {
+          alert ("General advice and assistance provided in relaton to " + topic + " completed benefit calc and advised tenant to call back if any further assistance needed.")
+    }
+      
